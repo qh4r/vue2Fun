06f1ad970105deab314f74830e675f4e331de8f4
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-12-04 12:22:34 +0100

    Vue routing and lifecycle test

---
+++ vueRoutingTest/src/pages/About.vue
@@ -0,0 +1,21 @@
+<template>
+    <main-layout>
+        <p>About</p>
+    </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+  export default {
+    components: {
+      MainLayout
+    },
+    beforeMount: () =>  console.log('rendered about'),
+    beforeDestroy: () => {
+      console.log('about destroyed');
+    }
+  }
+
+
+
+</script>
---
+++ vueRoutingTest/src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <main-layout>
+        <p>Home</p>
+    </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+  console.log('rendered home');
+  export default {
+    components: {
+      MainLayout
+    },
+    beforeDestroy: () => {
+      console.log('home destroy')
+    },
+    beforeMount: () => {
+      console.log('rendered home');
+    }
+
+  }
+
+
+</script>
