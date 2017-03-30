c07a37008464266c91332e3cc98bfb1a34c08c35
Rafał Kucharski <r.kucharski@lgbs.pl>
2017-03-30 22:44:53 +0200

    Fun with vue forms and inputs

--- vueWithCli/src/App.vue
+++ vueWithCli/src/App.vue
@@ -1,66 +1,70 @@
 <template>
-  <div id="app">
-    <h2>App app ap</h2>
-    <p>{{msg}}</p>
-    <div class="servers">
-    <status-view></status-view>
-    <status-view></status-view>
-    <status-view></status-view>
+    <div id="app">
+        <h2>App app ap</h2>
+        <p>{{msg}}</p>
+        <div class="servers">
+            <status-view></status-view>
+            <status-view></status-view>
+            <status-view></status-view>
+        </div>
+        <data-view></data-view>
+        <forms-view></forms-view>
     </div>
-    <data-view></data-view>
-  </div>
 </template>
 
 <script>
-  import Status from "./components/Status.vue";
-  import Data from "./components/Data.vue"
-  export default {
+    import Status from "./components/Status.vue";
+    import Data from "./components/Data.vue";
+    import Forms from "./components/Forms.vue";
+
+    export default {
 //  name: 'app', // opcjonalne
-    data (data) {
-      return {msg: "Welcome!"}
-    },
-    components:{
-      "status-view": Status,
-      "data-view": Data
-    },
-    // to zadzialalo by tylko jesli nie bylo by templatu dodanego!
+        data (data) {
+            return {msg: "Welcome!"}
+        },
+        components: {
+            "status-view": Status,
+            "data-view": Data,
+            "forms-view": Forms
+        },
+        // to zadzialalo by tylko jesli nie bylo by templatu dodanego!
 //    render: h => h(Start)
-  }
+    }
 </script>
 
 <style type="text/sass" lang="sass">
-  #app
-    font-family: 'Avenir', Helvetica, Arial, sans-serif
-    -webkit-font-smoothing: antialiased
-    -moz-osx-font-smoothing: grayscale
-    text-align: center
-    color: #2c3e50
-    margin-top: 60px
-    h2
-      color: blue
+    #app
+        font-family: 'Avenir', Helvetica, Arial, sans-serif
+        -webkit-font-smoothing: antialiased
+        -moz-osx-font-smoothing: grayscale
+        text-align: center
+        color: #2c3e50
+        margin-top: 60px
+        h2
+            color: blue
 
-  h1, h2
-    font-weight: normal
+        h1, h2
+            font-weight: normal
 
-  ul
-    list-style-type: none
-    padding: 0
+        ul
+            list-style-type: none
+            padding: 0
 
-  li
-    display: inline-block
-    margin: 0 10px
+        li
+            display: inline-block
+            margin: 0 10px
 
-  a
-    color: #42b983
+        a
+            color: #42b983
 
-  .servers
-    display: flex
-    flex-direction: row
-    flex-wrap: nowrap
-    align-content: space-between
-    >*
-      width: 100%
-      flex-grow: 1
+        .servers
+            display: flex
+            flex-direction: row
+            flex-wrap: nowrap
+            align-content: space-between
+            > *
+                width: 100%
+                flex-grow: 1
 
 
 </style>
---
+++ vueWithCli/src/components/Forms.vue
@@ -0,0 +1,164 @@
+<template>
+    <div>
+        <div class="container">
+            <form>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <h1>Zarzalenie</h1>
+                        <hr>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" v-model="user.email" class="form-control" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" v-model="user.password" class="form-control" id="password">
+                        </div>
+                        <div class="form-group">
+                            <label for="age"></label>
+                            <input type="number" v-model="user.age" class="form-control" id="age">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                        <label for="message">Wiadmość</label><br>
+                        <textarea
+                                id="message"
+                                rows="5"
+                                v-model="message"
+                                class="form-control"></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <!--checkboxy mapuja sie w tablice! jesli jest > niz jeden pasujacy do 1 pola w modelu-->
+                        <div class="form-group">
+                            <label for="sendmail">
+                                <input
+                                        type="checkbox"
+                                        id="sendmail"
+                                        value="SendMail"
+                                        v-model="sendMail"
+                                > Wyślij mail</label>
+                            <label for="sendInfomail">
+                                <input
+                                        type="checkbox"
+                                        id="sendInfomail"
+                                        value="SendInfo"
+                                        v-model="sendMail"
+                                > Wyślij informacje</label>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                        <!--radio matchuje po name-->
+                        <label for="male">
+                            <input
+                                    type="radio"
+                                    id="male"
+                                    name="male"
+                                    value="Male"
+                                    v-model="gender"
+                            > Mężczyzna </label>
+                        <label for="female">
+                            <input
+                                    type="radio"
+                                    id="female"
+                                    name="male"
+                                    value="Female"
+                                    v-model="gender"
+                            > Kobieta </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                        <label for="priority">Priorytet</label>
+                        <!--W PRZYPADKU SELECTA UStAWIAMY MODEL NA SELEKCIE WLSNIE-->
+                        <select v-model="selectedPriority"
+                                id="priority"
+                                class="form-control">
+                            <option v-for="priority in priorities"
+                                    > {{priority}}</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <!--POJEDYNCZY CHECKBOX MATCHUJE SIE  NA TRUE /FALSE-->
+                    <label for="newsletter"><input id="newsletter" v-model="newsletter" type="checkbox">
+                        Zapisz do newslettera</label>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <button
+                                class="btn btn-primary">Wyślij!
+
+
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>Twoje Dane</h4>
+                        </div>
+                        <div class="panel-body">
+                            <p>Mail: {{user.email}}</p>
+                            <p>Hasło: {{user.password}}</p>
+                            <p>Wiek: {{user.age}}</p>
+                            <p>Wiadomość: {{shortMessage}}</p>
+                            <p><strong>Wysłać mail?</strong>
+                            </p>
+                            <ul>
+                                <li v-for="item in sendMail">
+                                    {{item}}
+                                </li>
+                            </ul>
+                            <p>Subskrybjca: {{newsletter}}</p>
+                            <p>Płeć: {{gender}}</p>
+                            <p>Priorytet: {{selectedPriority}}</p>
+                            <p>Zmieniono:</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                user: {
+                    email: "start@asd.pl",
+                    password: "",
+                    age: ""
+                },
+                message: "msg\n\n\thehe",
+                sendMail: [],
+                newsletter: false,
+                gender: "",
+                priorities: ["Wysoki", "średni", "Niski"],
+                selectedPriority: "średni"
+            }
+        },
+        computed: {
+            shortMessage(){
+                return this.message.length > 25 ? `${this.message.substring(0, 25).trim()}...` : this.message;
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    textarea {
+        resize: none;
+    }
+</style>
