4164fad5dada882518ce76f41a7f9ee7bf6659f6
Rafa� Kucharski <rafal.kucharski@ac-project.net>
2017-04-01 23:13:19 +0200

    Played around with directives and their modifiers some more

--- vue_directives/src/App.vue
+++ vue_directives/src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="container">
-    <p>Blok <span v-mark="'#FFDC0C'">tesktu</span> tak dla <span v-mark="'#040528'"> demonstracji </span> dyrektyw</p>
+    <!--delayed to modyfikator-->
+    <p>Blok <span v-mark.animate.delayed="'#FFDC0C'">tesktu</span> tak dla <span v-bold v-mark="'#040528'"> demonstracji </span> dyrektyw</p>
+    <h1 v-mark.delayed="'#ff0309'">Takze test</h1>
+    <h1 v-bold.blink="{from: 100, to: 900}">i test animowany</h1>
+    <h1 v-bold.blink>i test animowany default</h1>
   </div>
 </template>
 
 <script>
 export default {
+  // zamiast globalnie mozna deklarowac per komponent w taki sposob
+  directives: {
+    "bold": {
+      bind(el, {value, modifiers}, vnode) {
+        el.style.fontWeight = 700;
+        if(modifiers["blink"]) {
+          let {from = 300, to = 700} = value || {};
+            setInterval(() => {
+            el.style.fontWeight = el.style.fontWeight == to ? from : to;
+          },1000)
+        }
+      }
+    }
+  },
   data () {
     return {
     }
--- vue_directives/src/main.js
+++ vue_directives/src/main.js
@@ -1,26 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-function getOpposite(hex){
+function getOpposite(hex) {
   let opposite = (max - Number.parseInt(`0x${hex}`)).toString(16);
   return opposite.length == 1 ? `0${opposite}` : opposite;
+}
+
+function performMark(el, binding) {
+  if (binding.value && binding.value.length == 7) {
+    let rgb = [
+      binding.value.substring(1, 3),
+      binding.value.substring(3, 5),
+      binding.value.substring(5, 7)
+    ].map(x => getOpposite(x)).join('');
+    console.log(`#${rgb}`);
+    el.style.color = `#${rgb}`;
+  }
+  el.style.backgroundColor = binding.value;
 }
 const max = 0xFF;
 Vue.directive('mark', {
   bind(el, binding, vnode){
-    console.log("dane", binding);
-    if (binding.value && binding.value.length == 7) {
-      let rgb = [
-        binding.value.substring(1, 3),
-        binding.value.substring(3, 5),
-        binding.value.substring(5, 7)
-      ].map(x => getOpposite(x)).join('');
-      console.log(`#${rgb}`);
-      el.style.color = `#${rgb}`;
+    if (binding.modifiers["delayed"]) {
+      binding.modifiers["animate"] && (el.style.transition = "all 2s");
+      return setTimeout(() => {
+        performMark(el, binding);
+      }, 2000);
     }
-    el.style.backgroundColor = binding.value;
+    performMark(el, binding);
   },
-
 });
 
 new Vue({
