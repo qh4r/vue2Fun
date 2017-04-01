1ef4458f9fad32e36edb580289f7d84c20e47f6e
Rafa� Kucharski <rafal.kucharski@ac-project.net>
2017-04-02 00:46:33 +0200

    Played around with vue filters

---
+++ vue_mixins_and_filters/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h1>mixiny itd:</h1>
+    <!--filtry mozna laczyc w kolejki-->
+    <p> {{example | toUpper | reverse}}</p>
+    <list-view></list-view>
+  </div>
+</template>
+
+<script>
+  import List from "./List.vue"
+  export default {
+    components: {
+      "list-view": List
+    },
+    data () {
+      return {
+        example: "example text",
+      }
+    },
+    //mozna tez rejestrowac globalnie tak jak komponenty Vue.filter("asd", fun())
+    filters: {
+      toUpper(x) {
+        return x.toUpperCase();
+      },
+      reverse(x) {
+        return x.split('').reverse().join('')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  input.search {
+    display: block;
+  }
+</style>
---
+++ vue_mixins_and_filters/src/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <!--w vue 2 nie mozna przekazywac parametrow z zewnatrz do filtrow-->
+    <input class="search" placeholder=Filtruj  type="text" v-model="filterQuote">
+    <li v-for="name in matchingNames">{{name}}</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        filterQuote: "",
+        names: ["Rafał", "Arek", "Grzegorz", "Asia", "Marek", "Darek", "Ola", "Romek"]
+      }
+    },
+    computed: {
+      matchingNames: function () {
+        return this.names.filter(x => x.toLowerCase().indexOf(this.filterQuote) > -1)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
