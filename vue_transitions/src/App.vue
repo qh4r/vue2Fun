7e239f9c70ed7e1a9bf4defacb6e51bc167e1edb
Rafa� Kucharski <rafal.kucharski@ac-project.net>
2017-04-02 16:56:52 +0200

    Some basic animations usig vue build in component

--- vue_transitions/src/App.vue
+++ vue_transitions/src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div class="container">
-    asd
+    <div class="row">
+      <button @click="toggle" class="btn btn-primary">Pokaż ukryte</button>
+      <br/>
+      <br/>
+      <!--w transition mozna miec tylko 1 element root!-->
+      <transition name="fade">
+        <div v-if="secretVisible" class="alert alert-info">
+          To tajemnica
+        </div>
+      </transition>
+
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {}
+  export default {
+    data () {
+      return {
+        secretVisible: false
+      }
+    },
+    methods: {
+      toggle(){
+        this.secretVisible = !this.secretVisible;
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss">
+  /* nienazwane animacje dzialaja na v-enter i v-leave*/
+  .fade-enter {
+    opacity: 0;
+  }
 
+  .fade-enter-active {
+    transition: opacity 1s;
+    opacyity: 1;
+  }
+
+  .fade-leave {
+    opacyity: 1;
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
 </style>
