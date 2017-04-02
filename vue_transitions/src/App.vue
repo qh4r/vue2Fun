eabb000a25e293597ddf2f677a62ca5311fb88e7
Rafa� Kucharski <rafal.kucharski@ac-project.net>
2017-04-02 15:41:37 +0200

    Created next boilerplate

---
+++ vue_transitions/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    asd
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
