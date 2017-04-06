548d987ab245fc87f3442c51669a38f1ace64e76
Rafał Kucharski <r.kucharski@lgbs.pl>
2017-04-06 23:14:01 +0200

    Palyed around with animation callbacks and js animation in vue

--- vue_transitions/src/App.vue
+++ vue_transitions/src/App.vue
@@ -1,136 +1,284 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <button @click="toggle" class="btn btn-primary">Pokaż ukryte</button>
-      <br/>
-      <br/>
-      <!--w transition mozna miec tylko 1 element root!-->
-      <transition name="fade">
-        <!--Name moze byc ustawiany dynamicznie!!-->
-        <div v-if="secretVisible" class="alert alert-info">
-          To tajemnica
-        </div>
-      </transition>
-      <!-- animacja dziala tak samo z v-show-->
-      <transition name="slide">
-        <div v-show="secretVisible" class="alert alert-info">
-          To też
-        </div>
-      </transition>
-      <!--
-        moze byc jeszcze type transition - type pomaga zdecydowac vue ktora dlugosc animacji przyjac
-        jako roboczy czas trwania animacji (sluzy do eventow itd) - to co podamy w type bedzie czasem animacji jesli uzywamy
-        i animacji i transition tak jak w typie mix
-      -->
-      <transition name="mix" type="animation" appear>
-        <!--
-          appear sprawia ze animacja bedzie odgrywana podczas poczatkowego zaladowania!
-          zeby zadzialala podczas zaladowania element musi byc ustawiony jako widoczny - (stad odwrocony warunek)
-        -->
-        <div v-if="!secretVisible" class="alert alert-info">
-          No i to jeszcze
-        </div>
-      </transition>
+    <div class="container">
+        <div class="row">
+            <button @click="toggle" class="btn btn-primary">Pokaż ukryte</button>
+            <br/>
+            <br/>
+            <!--w transition mozna miec tylko 1 element root!-->
+            <transition name="fade">
+                <!--Name moze byc ustawiany dynamicznie!!-->
+                <div v-if="secretVisible" class="alert alert-info">
+                    To tajemnica
 
-      <!--MOZNA NADPISAC DOMYSLNE UZYWANE KLASY (enter, enter-active itd), sa jeszcze:-->
-      <!--leave-class=""-->
-      <!--enter-class=""-->
-      <transition
-              enter-active-class="animated bounce"
-              leave-active-class="animated shake"
-      >
-        <div v-show="secretVisible" class="alert alert-info">
-          no i z super animacja
+
+
+
+                </div>
+            </transition>
+            <!-- animacja dziala tak samo z v-show-->
+            <transition name="slide">
+                <div v-show="secretVisible" class="alert alert-info">
+                    To też
+
+
+
+
+                </div>
+            </transition>
+            <!--
+              moze byc jeszcze type transition - type pomaga zdecydowac vue ktora dlugosc animacji przyjac
+              jako roboczy czas trwania animacji (sluzy do eventow itd) - to co podamy w type bedzie czasem animacji jesli uzywamy
+              i animacji i transition tak jak w typie mix
+            -->
+            <transition name="mix" type="animation" appear>
+                <!--
+                  appear sprawia ze animacja bedzie odgrywana podczas poczatkowego zaladowania!
+                  zeby zadzialala podczas zaladowania element musi byc ustawiony jako widoczny - (stad odwrocony warunek)
+                -->
+                <div v-if="!secretVisible" class="alert alert-info">
+                    No i to jeszcze
+
+
+
+
+                </div>
+            </transition>
+
+            <!--MOZNA NADPISAC DOMYSLNE UZYWANE KLASY (enter, enter-active itd), sa jeszcze:-->
+            <!--leave-class=""-->
+            <!--enter-class=""-->
+            <transition
+                    enter-active-class="animated bounce"
+                    leave-active-class="animated shake"
+            >
+                <div v-show="secretVisible" class="alert alert-info">
+                    no i z super animacja
+
+
+
+
+                </div>
+            </transition>
+
+            <!--!! TRANSITION MODE !!-->
+            <!--MODE OUT IN SPRAWIA ZE ELEMENTY NIE POKAUZJA SIE POD SOBA TYLKO WYMIENIAJA
+            (ANIMACJA 2 ZACZYNA SIE GDY KONCZY SIE ANIMACJA 1)
+            * mode in-out - sprawia ze wyjscie zaczyna sie dopiero gdy skonczy sie animacja wejscia
+            * domyslnie obie animacje startuja rownoczesnie-->
+
+            <!--jesli 2 elementy byly by widoczne naraz w transition to vue wyswietli tylko pierwszy-->
+            <transition name="fade" mode="out-in">
+                <!--jesli elementy sa tego samego typu to potrzebuja key! -->
+                <div key="1" v-if="secretVisible" class="alert alert-warning">TOGGLE 1/2</div>
+                <div key="2" v-else="secretVisible" class="alert alert-success">TOGGLE 2/2</div>
+            </transition>
+            <!--mozna togglowac kilka elementow ale tylko jesli 1 naraz jest widoczny-->
+            <hr>
+            <button class="btn btn-success" @click="cssVisible = !cssVisible">Toggle css animation</button>
+            <transition
+                    @before-enter="cssBeforeEnter"
+                    @enter="cssEnter"
+                    @after-enter="cssAfterEnter"
+                    @before-leave="cssBeforeLeave"
+                    @leave="cssLeave"
+                    @after-leave="cssAfterLeave"
+                    @leave-cancelled="cssLeaveCanceled"
+                    @enter-cancelled="cssEnterCanceled"
+                    name="mix">
+                <div v-if="cssVisible" class="alert alert-danger">
+                    Eventy eventy eventy
+
+
+                </div>
+            </transition>
+            <hr>
+            <button class="btn btn-warning" @click="jsVisible = !jsVisible">Toggle js animation</button>
+            <transition
+                    @before-enter="jsBeforeEnter"
+                    @enter="jsEnter"
+                    @after-enter="jsAfterEnter"
+                    @before-leave="jsBeforeLeave"
+                    @leave="jsLeave"
+                    @after-leave="jsAfterLeave"
+                    @leave-cancelled="jsLeaveCanceled"
+                    @enter-cancelled="jsEnterCanceled"
+                    :css="false">
+                <!--css false sprawia ze nie beda wykonywane animacje klas cssowych (o odpowiednich nazwach) w ogole-->
+                <div :style="{backgroundColor: 'red', width: '100px', height: '100px'}" v-if="jsVisible"
+                     class="alert alert-danger">
+
+                </div>
+            </transition>
         </div>
-      </transition>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        secretVisible: false
-      }
-    },
-    methods: {
-      toggle(){
-        this.secretVisible = !this.secretVisible;
-      }
+    export default {
+        data () {
+            return {
+                secretVisible: false,
+                cssVisible: false,
+                jsVisible: false,
+                boxWidth: 100,
+                enterLoop: null,
+                leaveLoop: null
+            }
+        },
+        methods: {
+            toggle(){
+                this.secretVisible = !this.secretVisible;
+            },
+            cssBeforeEnter(elem) {
+                console.log('before enter !', elem);
+            },
+//            enter(elem, done){
+            cssEnter(elem){
+                console.log('enter !', elem);
+                // done mozna uzywac jesli animujemy jsem a nie cssem
+
+                // done();
+                // JESLI PODAMY DONE W ARGUMENTACH TO MUSIMY JE WYWOLAC BO afterEnter NIGDY NIE NASTAPI
+                // JESLI POMINIEMY DONE W ARGUMENTACH METODY TO AFTER ENTER NASTAPI PO ZAKONCZENIU ANIMACJI
+            },
+            cssAfterEnter(elem) {
+                console.log('after enter !', elem);
+            },
+            cssBeforeLeave(elem) {
+                console.log('before leave !', elem);
+            },
+            cssLeave(elem) { // jak enter, tez przyjmuje done
+                console.log('leave!', elem);
+            },
+            cssAfterLeave(elem) {
+                console.log('after leave !', elem);
+            },
+            // metody wywolywane jesli enter albo leave zostanie przerwany
+            cssEnterCanceled(elem) {
+                console.log('canceled enter !', elem);
+            },
+            cssLeaveCanceled(elem) {
+                console.log('canceled leave !', elem);
+            },
+
+
+            jsBeforeEnter(elem) {
+                console.log('before enter !', elem);
+            },
+            jsEnter(elem, done){
+                this.enterLoop = setInterval(() => {
+                    elem.style.width = ++this.boxWidth + 'px';
+                    if (this.boxWidth > 999) {
+                        clearInterval(this.enterLoop);
+                        done();
+                    }
+                }, 1);
+                console.log('enter !', elem);
+            },
+            jsAfterEnter(elem) {
+                console.log('after enter !', elem);
+            },
+            jsBeforeLeave(elem) {
+                console.log('before leave !', elem);
+            },
+            jsLeave(elem, done) {
+                this.leaveLoop = setInterval(() => {
+                    elem.style.width = --this.boxWidth + 'px';
+                    if (this.boxWidth < 101) {
+                        clearInterval(this.leaveLoop );
+                        done();
+                    }
+                }, 1)
+                console.log('leave!', elem);
+            },
+            jsAfterLeave(elem) {
+                console.log('after leave !', elem);
+            },
+            jsEnterCanceled(elem) {
+                if(this.enterLoop){
+                    clearInterval(this.enterLoop)
+                }
+                console.log('canceled enter !', elem), this.enterLoop;
+            },
+            jsLeaveCanceled(elem) {
+                if(this.leaveLoop){
+                    clearInterval(this.leaveLoop)
+                }
+                console.log('canceled leave !', elem);
+            }
+        }
     }
-  }
 </script>
 
 <style lang="scss">
-  /* nienazwane animacje dzialaja na v-enter i v-leave*/
-  .fade-enter {
-    opacity: 0;
-  }
-
-  .fade-enter-active {
-    transition: opacity 1s;
-    opacyity: 1;
-  }
-
-  .fade-leave {
-    opacyity: 1;
-  }
-
-  .fade-leave-active {
-    transition: opacity 1s;
-    opacity: 0;
-  }
-
-  @keyframes slide-in {
-    from {
-      transform: translateY(20px);
+    /* nienazwane animacje dzialaja na v-enter i v-leave*/
+    .fade-enter {
+        opacity: 0;
     }
-    to {
-      transform: translateY(0);
+
+    .fade-enter-active {
+        transition: opacity 1s;
+        opacyity: 1;
     }
-  }
 
-  @keyframes slide-out {
-    from {
-      transform: translateY(0);
+    .fade-leave {
+        opacyity: 1;
     }
-    to {
-      transform: translateY(20px);
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
     }
-  }
 
-  .slide-enter {
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
 
-  }
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
 
-  .slide-enter-active {
-    animation: slide-in 1s ease-out forwards;
-  }
+    .slide-enter {
 
-  .slide-leave {
+    }
 
-  }
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
 
-  /*forwards sprawia ze animacja zostaje po zakonczeniu i nie wraca ani nie loopuje*/
-  .slide-leave-active {
-    animation: slide-out 1s ease-out forwards;
-  }
+    .slide-leave {
 
-  .mix-enter {
-    @extend .fade-enter;
-  }
+    }
 
-  .mix-enter-active {
-    @extend .slide-enter-active, .fade-enter-active
-  }
+    /*forwards sprawia ze animacja zostaje po zakonczeniu i nie wraca ani nie loopuje*/
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+    }
 
-  .mix-leave {
-    @extend .fade-leave
-  }
+    .mix-enter {
+        @extend .fade-enter;
+    }
 
-  .mix-leave-active {
-    @extend .slide-leave-active, .fade-leave-active
-  }
+    .mix-enter-active {
+        @extend .slide-enter-active, .fade-enter-active
+    }
+
+    .mix-leave {
+        @extend .fade-leave
+    }
+
+    .mix-leave-active {
+        @extend .slide-leave-active, .fade-leave-active
+    }
 
 </style>
