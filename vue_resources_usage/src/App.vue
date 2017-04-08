5962a1e73454e25376baa028680b87d2e402de61
Rafał Kucharski <r.kucharski@lgbs.pl>
2017-04-08 14:26:36 +0200

    Started looking into vue resource

---
+++ vue_resources_usage/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h1>Formularz</h1>
+            <div class="form-group">
+                <label>User</label>
+                <input type="text" v-model="username" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Mail</label>
+                <input type="text" v-model="email" class="form-control">
+            </div>
+        </div>
+        <button @click="submit" class="btn btn-info ">Wyslij</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                username: '',
+                email: ''
+            }
+        },
+        methods: {
+            submit(){
+                // dostepne dzieki vue resource
+                this.$http.post('https://vue-fun.firebaseio.com/data.json', {
+                    username: this.username,
+                    email: this.email
+                }).then((response) => {
+                    console.log(response)
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
